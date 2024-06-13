359d9d10e2db0121e762765c8bbe7eacb65cf7af
Dhaval Solanki <dhaval24@gmail.com>
2024-06-13 11:46:52 +0530

    HI

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,41 @@ using namespace std;
 int main(){
     int a=65;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
      cout<<"hello"<<char(a)<<endl;
      cin>>a;
      cout<<"hello"<<a; 
