1c2babf46136414f80ad73b3938639679718205f
Dhaval Solanki <dhaval24@gmail.com>
2024-06-15 11:55:17 +0530

    hi

--- practice10.cpp
+++ practice10.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+     int i=0;
+        while(i<5){
+             int j=0;
+                while(j<=i){
+                    cout<<"*";
+                    j++;
+                }
+                i++;
+                cout<<endl;
+        }
     return 0;
 }
