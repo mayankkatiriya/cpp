0ca074139c5c649b72d07d11d1712ac74b1ca6e1
Dhaval Solanki <dhaval24@gmail.com>
2024-06-17 10:15:45 +0530

    hi

---
+++ practice11.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+    using namespace std;
+
+    int main(){
+
+        float unit,rs,min,total=0;
+
+        int charge=50;
+
+        cout<<"enter unit:-";
+        cin>>unit;
+
+        cout<<endl;
+
+        if(unit <= 100 && unit >50)
+        {
+            rs = 0.60;
+            
+        }
+        else if(unit > 100 && unit <= 200)
+        {
+            rs = 0.80;
+            cout<<unit;
+        }
+        else if(unit > 200 && unit <= 300)
+        {
+            rs = 0.90;
+            cout<<unit;
+        }else{
+
+            rs=0.90+15/100;
+        }
+
+        total = unit * rs + charge;
+
+        cout<<endl;
+
+        cout<<total<<endl;
+
+        return 0;
+    }
