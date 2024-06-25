84fe164683bfd7660aaad9880bd92d210b230863
Dhaval Solanki <dhaval24@gmail.com>
2024-06-25 10:54:08 +0530

    hii

---
+++ operatoroverloding1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+    using namespace std;
+    class complex{
+        int x,y;
+
+        public:
+        complex(){
+            x=0;
+            y=0;
+        }
+
+        complex(int a,int b){
+            x=a;
+            y=b;
+        }
+            void desplay(){
+                cout<<"value of x : "<<x<<endl;
+                cout<<"value of y : "<<y<<endl;
+
+            }
+            complex operator-(complex e){
+                complex t;
+                t.x=x-e.x;
+                t.y=y-e.y;
+                return t;
+            }
+    };
+    int main(){
+        complex s1,s2,s3;
+        s1=complex(3,4);
+        s2=complex(2,6);
+
+        cout<<"first obj"<<endl;
+        s1.desplay();
+
+        cout<<"secound obj"<<endl;
+         s2.desplay();
+
+         cout<<"final output"<<endl;
+
+         s3=s1-s2;
+         s3.desplay();
+    }
