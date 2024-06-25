4eb443b164fed5b16423a38c5e172749f4bec689
Dhaval Solanki <dhaval24@gmail.com>
2024-06-25 10:14:27 +0530

    hii

--- polymorphism2.cpp
+++ polymorphism2.cpp
@@ -1,32 +1,24 @@
 #include<iostream>
 using namespace std;
-class demo
-{
+class demo{
     public:
     void Area(int a)
     {
         cout<<"Area of square:"<<a*a<<endl;
     }
-    void Area(int a,int b)
-    {
+    void Area(int a,int b){
         cout<<"Area of rectangle:"<<a*b<<endl;
     }
-    void Area(double a)
-    {
-        float pi=3.14;
-        cout<<"Area of circle:"<<pi*a*a<<endl;
+    void Area(double a){
+        cout<<"Area of circle:"<<3.14*a*a<<endl;
     }
-    void Area(double a,int b)
-    {
-        
+    void Area(double a,int b){
         cout<<"Area of triangle:"<<0.5*a*b<<endl;
     }
 };
-int main()
-{
+int main(){
     class demo a;
         a.Area(10,10);
         a.Area(10.0);
         a.Area(10.0,10);
-    return 0;
 }
